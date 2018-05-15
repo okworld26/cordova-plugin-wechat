f39b4e0050f8d18cd340d80cfb34fba450343b7d
Stephen <jiangtao@cposp.com>
2018-05-15 19:13:46 +0800

    修复Wechat的getAppid方法签名错误

--- src/android/Wechat.java
+++ src/android/Wechat.java
@@ -278,7 +278,7 @@ public class Wechat extends CordovaPlugin {
 
         try {
             final String appid = params.getString("appid");
-            final String savedAppid = getAppId(cordova.getActivity());
+            final String savedAppid = getAppId();
             if (!savedAppid.equals(appid)) {
                 this.saveAppId(cordova.getActivity(), appid);
             }
@@ -595,7 +595,7 @@ public class Wechat extends CordovaPlugin {
         return null;
     }
 
-    public static String getAppId() {
+    public String getAppId() {
         if (appId == null) {
             appId = preferences.getString(WXAPPID_PROPERTY_KEY, "");
         }
